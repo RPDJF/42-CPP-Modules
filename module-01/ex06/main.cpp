7f67f75f450aceeaffabfa99b047aab1752e164f
Rui Paulo <86334233+RPDJF@users.noreply.github.com>
2024-05-17 14:30:00 +0200

    Added ex06

---
+++ module-01/ex06/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "classes/Harl.hpp"
+
+int	main(int argc, char **argv) {
+	Harl harl;
+
+	if (argc != 2) {
+		std::cerr << "Usage: ./harl [DEBUG/INFO/WARNING/ERROR]\n" << std::endl;
+		return 1;
+	}
+	for(int i = 0; argv[1][i]; i++)
+		argv[1][i] = tolower(argv[1][i]);
+	harl.complain(argv[1]);
+} 
