e0f4987f4b14da405bf2be8287875b910aba05fe
Rui Paulo <86334233+RPDJF@users.noreply.github.com>
2024-05-17 14:18:10 +0200

    Refactor debug, info, warning, error, and complain functions in Harl class

--- module-01/ex05/classes/Harl.cpp
+++ module-01/ex05/classes/Harl.cpp
@@ -1,20 +1,30 @@
 #include "Harl.hpp"
 
 void	Harl::debug() {
-	std::cout << "\"DEBUG\" level: Debug messages contain contextual information. They are mostly used for problem diagnosis.\nExample: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\"" << std::endl;
+	std::cout << "[ DEBUG ]\n"
+		<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+		<< std::endl;
 }
 
 void	Harl::info() {
-	std::cout << "\"INFO\" level: These messages contain extensive information. They are helpful for tracing program execution in a production environment.\nExample: \"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\"" << std::endl;
+	std::cout << "[ INFO ]\n"
+		<< "I cannot believe adding extra bacon costs more money.\n"
+		<< "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+		<< std::endl;
 }
 
 void Harl::warning() {
-	std::cout << "\"WARNING\" level: Warning messages indicate a potential issue in the system.\nHowever, it can be handled or ignored.\nExample: \"I think I deserve to have some extra bacon for free. I’ve been coming foryears whereas you started working here since last month.\"" << std::endl;
+	std::cout << "[ WARNING ]\n"
+		<< "I think I deserve to have some extra bacon for free.\n"
+		<< "I’ve been coming foryears whereas you started working here since last month."
+		<< std::endl;
 }
 
 
 void Harl::error() {
-	std::cout << "\"ERROR\" level: These messages indicate an unrecoverable error has occurred.\nThis is usually a critical issue that requires manual intervention.\nExample: \"This is unacceptable! I want to speak to the manager now.\"" << std::endl;
+	std::cout << "[ ERROR ]\n"
+		<< "This is unacceptable, I want to speak to the manager now."
+		<< std::endl;
 }
 
 void	Harl::complain(std::string level) {
@@ -27,5 +37,7 @@ void	Harl::complain(std::string level) {
 			return;
 		}
 	}
-	std::cout << "There is no such thing !" << std::endl;
+	std::cout << "[ IDIOT DETECTED ]\n"
+		<< "There is no such thing !"
+		<< std::endl;
 }
