294087e77f088ceec93c8601a9863714718be325
RPDJF <ruipaulo.unify@outlook.fr>
2025-01-16 23:26:30 +0100

    working on ex02 from module-05

---
+++ module-05/ex02/classes/Bureaucrat.hpp
@@ -0,0 +1,39 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include "./AForm.hpp"
+# include <iostream>
+# include "../utils/colors.h"
+
+class AForm;
+
+class Bureaucrat {
+	private:
+		std::ostream& annonce() const;
+		const std::string name_;
+		int grade_;
+		// exceptions
+		class GradeTooLowException: public std::exception { virtual const char *what() const throw(); };
+		class GradeTooHighException: public std::exception { virtual const char *what() const throw(); };
+	public:
+		static const int MIN_GRADE;
+		static const int MAX_GRADE;
+
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& copy);
+		Bureaucrat& operator=(const Bureaucrat& assign);
+		~Bureaucrat();
+		const std::string& getName() const;
+		bool operator<(const Bureaucrat& ref) const;
+		bool operator<(int grade) const;
+		bool operator>(const Bureaucrat& ref) const;
+		bool operator>(int grade) const;
+		int getGrade() const;
+		void signForm(AForm& form) const;
+		void executeForm(AForm& form) const;
+};
+
+std::ostream& operator<<(std::ostream& stream, const Bureaucrat& bureaucrat);
+
+# endif
---
+++ module-05/ex02/classes/ShrubberyCreationForm.hpp
@@ -0,0 +1,23 @@
+#ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
+
+# include "./AForm.hpp"
+# include <fstream>
+
+class ShrubberyCreationForm: public AForm {
+	private:
+		std::ostream& annonce() const;
+		static const int REQ_SIGN_GRADE_ = 145;
+		static const int REQ_EXEC_GRADE_ = 137;
+		const std::string target_;
+		void createShrubberyFile() const;
+	public:
+		ShrubberyCreationForm();
+		ShrubberyCreationForm(const std::string& name);
+		ShrubberyCreationForm(const ShrubberyCreationForm& copy);
+		~ShrubberyCreationForm();
+		ShrubberyCreationForm& operator=(const ShrubberyCreationForm& assign);
+		void execute(const Bureaucrat& executor);
+};
+
+#endif
