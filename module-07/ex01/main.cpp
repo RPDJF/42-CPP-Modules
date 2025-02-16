fce6d312fe08b995bd3efdb81868a827b1e74530
RPDJF <ruipaulo.unify@outlook.fr>
2025-02-16 15:23:44 +0100

    Addex ex01 from module 07

---
+++ module-07/ex01/main.cpp
@@ -0,0 +1,33 @@
+#include "utils/colors.h"
+#include "utils/MyStd.hpp"
+#include <iostream>
+
+template<typename T>
+static void print(T var) {
+	std::cout << var << " ";
+}
+
+static void basicTests() {
+	std::cout << C_YELLOW << "RUNNING BASICTESTS() FOR STRINGS" << C_RESET << std::endl;
+	{
+		int arr[] = {0, 3, 1, 5, 8,-1, 24};
+		MyStd::iter(arr, 7, &print);
+		std::cout << std::endl;
+	}
+	{
+		std::string arr[] = {"hello", "world", "how", "are","you", "today", "?"};
+		MyStd::iter(arr, 7, &print);
+		std::cout << std::endl;
+	}
+	std::cout << C_YELLOW << "ENDING BASICTESTS()" << C_RESET << std::endl << std::endl;
+}
+
+static int runTests() {
+	basicTests();
+	return 0;
+}
+
+int main() {
+	runTests();
+	//ScalarConverter::convert(argv[1]);
+}
