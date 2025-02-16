6a46f4f7d239cfc4626fb453f126445e7b5f3958
RPDJF <ruipaulo.unify@outlook.fr>
2025-02-16 15:10:47 +0100

    added exercice 00 module 07

---
+++ module-07/ex00/main.cpp
@@ -0,0 +1,131 @@
+#include "utils/colors.h"
+#include "utils/MyStd.hpp"
+#include <iostream>
+
+template<typename T>
+static void swap(T& v1, T& v2) { MyStd::swap(v1, v2); }
+template<typename T>
+static const T& min(const T& v1, const T& v2) { return MyStd::min(v1, v2); }
+template<typename T>
+static const T& max(const T& v1, const T& v2) { return MyStd::max(v1, v2); }
+
+static void swapTests() {
+	std::cout << C_YELLOW << "RUNNING SWAPTESTS() FOR STRINGS" << C_RESET << std::endl;
+	{
+
+		std::string str1("hello");
+		std::string str2("world");
+		std::cout << C_CYAN << "str1: " << C_RESET << str1 << " " << C_CYAN << "str2: " << C_RESET << str2 << std::endl;
+		std::cout << C_MAGENTA << "swaping!" << C_RESET << std::endl;
+		::swap(str1, str2);
+		std::cout << C_CYAN << "str1: " << C_RESET << str1 << " " << C_CYAN << "str2: " << C_RESET << str2 << std::endl;
+	}
+	std::cout << C_YELLOW << "RUNNING SWAPTESTS() FOR INTEGERS" << C_RESET << std::endl;
+	{
+
+		int int1 = 42;
+		int int2 = 24;
+		std::cout << C_CYAN << "int1: " << C_RESET << int1 << " " << C_CYAN << "int2: " << C_RESET << int2 << std::endl;
+		std::cout << C_MAGENTA << "swaping!" << C_RESET << std::endl;
+		::swap(int1, int2);
+		std::cout << C_CYAN << "int1: " << C_RESET << int1 << " " << C_CYAN << "int2: " << C_RESET << int2 << std::endl;
+	}
+	std::cout << C_YELLOW << "ENDING SWAPTESTS()" << C_RESET << std::endl << std::endl;
+}
+
+static void minTests() {
+	std::cout << C_YELLOW << "RUNNING MINTESTS() FOR STRINGS" << C_RESET << std::endl;
+	{
+
+		std::string str1("hello");
+		std::string str2("world");
+		std::cout << C_CYAN << "str1: " << C_RESET << str1 << " " << C_CYAN << "str2: " << C_RESET << str2 << std::endl;
+		std::cout << C_MAGENTA << "Returning: " << ::min(str1, str2) << C_RESET << std::endl;
+	}
+	std::cout << C_YELLOW << "RUNNING MINTESTS() FOR INTEGERS" << C_RESET << std::endl;
+	{
+
+		int int1 = 42;
+		int int2 = 24;
+		std::cout << C_CYAN << "int1: " << C_RESET << int1 << " " << C_CYAN << "int2: " << C_RESET << int2 << std::endl;
+		std::cout << C_MAGENTA << "Returning: " << ::min(int1, int2) << C_RESET << std::endl;
+	}
+	{
+		int int1 = 12;
+		int int2 = 52;
+		std::cout << C_CYAN << "int1: " << C_RESET << int1 << " " << C_CYAN << "int2: " << C_RESET << int2 << std::endl;
+		std::cout << C_MAGENTA << "Returning: " << ::min(int1, int2) << C_RESET << std::endl;
+	}
+		{
+		int int1 = 12;
+		int int2 = 12;
+		std::cout << C_CYAN << "int1 (" << &int1 << "): " << C_RESET << int1 << " " << C_CYAN << "int2 (" << &int2 << "): " << C_RESET << int2 << std::endl;
+		std::cout << C_MAGENTA << "Returning address: " << &::min(int1, int2) << C_RESET << std::endl;
+	}
+	std::cout << C_YELLOW << "ENDING MINTESTS()" << C_RESET << std::endl << std::endl;
+}
+
+static void maxTests() {
+	std::cout << C_YELLOW << "RUNNING MAXTESTS() FOR STRINGS" << C_RESET << std::endl;
+	{
+
+		std::string str1("hello");
+		std::string str2("world");
+		std::cout << C_CYAN << "str1: " << C_RESET << str1 << " " << C_CYAN << "str2: " << C_RESET << str2 << std::endl;
+		std::cout << C_MAGENTA << "Returning: " << ::max(str1, str2) << C_RESET << std::endl;
+	}
+	std::cout << C_YELLOW << "RUNNING MAXTESTS() FOR INTEGERS" << C_RESET << std::endl;
+	{
+
+		int int1 = 42;
+		int int2 = 24;
+		std::cout << C_CYAN << "int1: " << C_RESET << int1 << " " << C_CYAN << "int2: " << C_RESET << int2 << std::endl;
+		std::cout << C_MAGENTA << "Returning: " << ::max(int1, int2) << C_RESET << std::endl;
+	}
+	{
+		int int1 = 12;
+		int int2 = 52;
+		std::cout << C_CYAN << "int1: " << C_RESET << int1 << " " << C_CYAN << "int2: " << C_RESET << int2 << std::endl;
+		std::cout << C_MAGENTA << "Returning: " << ::max(int1, int2) << C_RESET << std::endl;
+	}
+	{
+		int int1 = 12;
+		int int2 = 12;
+		std::cout << C_CYAN << "int1 (" << &int1 << "): " << C_RESET << int1 << " " << C_CYAN << "int2 (" << &int2 << "): " << C_RESET << int2 << std::endl;
+		std::cout << C_MAGENTA << "Returning address: " << &::max(int1, int2) << C_RESET << std::endl;
+	}
+	std::cout << C_YELLOW << "ENDING MAXTESTS()" << C_RESET << std::endl << std::endl;
+}
+
+static void mandatoryTests() {
+	std::cout << C_YELLOW << "RUNNING MANDATORYTESTS() FOR INTEGERS" << C_RESET << std::endl;
+	{
+		int a = 2;
+		int b = 3;
+
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+	std::cout << C_YELLOW << "ENDING MANDATORYTESTS()" << C_RESET << std::endl << std::endl;
+}
+
+static int runTests() {
+	swapTests();
+	minTests();
+	maxTests();
+	mandatoryTests();
+	return 0;
+}
+
+int main() {
+	runTests();
+	//ScalarConverter::convert(argv[1]);
+}
---
+++ module-07/ex00/utils/MyStd.hpp
@@ -0,0 +1,36 @@
+#ifndef MYSTD_HPP
+# define MYSTD_HPP
+
+#include <string>
+#include <ostream>
+#include <sstream>
+
+class MyStd {
+	public:
+		template<typename T>
+		static std::string to_string(const T& value)  {
+			std::ostringstream oss;
+			oss << value;
+			return oss.str();
+		}
+		template<typename T>
+		static void swap(T& v1, T& v2) {
+			T swap = v1;
+			v1 = v2;
+			v2 = swap;
+		}
+		template<typename T>
+		static const T& min(const T& v1, const T& v2) {
+			if (v1 < v2)
+				return v1;
+			return v2;
+		}
+		template<typename T>
+		static const T& max(const T& v1, const T& v2) {
+			if (v1 > v2)
+				return v1;
+			return v2;
+		}
+};
+
+#endif
